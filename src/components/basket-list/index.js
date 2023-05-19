6eef50dd23ac9af3c58346ce69a8cf838ef85987
Sergey Kalachev <rollstream@yandex.ru>
2023-05-19 10:44:24 +0700

    refactored

--- src/components/basket-list/index.js
+++ src/components/basket-list/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import BasketItem from "../basket-item";
+import BasketItem from '../basket-item';
 import './style.css';
 
 function BasketList(props){
