1fe9e846cfbb4891c530ca2831b359171ec7060a
Sergey Kalachev <rollstream@yandex.ru>
2023-06-05 09:41:19 +0700

    styles fixed

--- src/components/layouts/side-layout/index.js
+++ src/components/layouts/side-layout/index.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import './style.css';
 
-function SideLayout({children, side, padding, theme}) {
+function SideLayout({children, side, padding}) {
   const cn = bem('SideLayout');
   return (
-    <div className={cn({side, padding, theme})}>
+    <div className={cn({side, padding})}>
       {React.Children.map(children, (child) => (
         <div key={child.key} className={cn('item')}>{child}</div>
       ))}
@@ -18,7 +18,6 @@ SideLayout.propTypes = {
   children: PropTypes.node,
   side: PropTypes.oneOf(['start', 'end', 'between']),
   padding: PropTypes.oneOf(['small', 'medium']),
-  theme: PropTypes.oneOf(['border']),
 }
 
 SideLayout.defaultProps = {};
